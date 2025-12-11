959e97c1de5b856fa3ff700edb2d00119f73fb58
Denys <ia.oliveira.click@gmail.com>
2025-12-10 21:09:49 -0300

    feat: update PWA icons and manifest name to Pedidos Bumn Ice

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+```
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Pedidos Bumn Ice',
+        short_name: 'Pedidos Bumn Ice',
+      }
+    })
+  ],
 })
+```
